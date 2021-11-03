01a734bf68b4bf07ade507faffcb5ffff8e3eb76
abant006 <aaronbantay@gmail.com>
2021-11-03 11:51:38 -0700

    Adding a .gitignore, CMakeLists.txt, and edited op.hpp

--- op.hpp
+++ op.hpp
@@ -4,10 +4,40 @@
 #include "base.hpp"
 
 class Op : public Base {
+
+    private:
+        double value;
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base() { this->value = value; }
+        virtual double evaluate() { return value; }
+	// sstream?
+        virtual std::string stringify() {
+	if (value == 0) {
+	return "0";
+	}
+	std::string str = std::to_string((value));
+   }
+};
+
+class OpPositiveMock : public Base {
+	public:
+		OpPositiveMock() { }
+		virtual double evaluate() { return 1.0; }
+		virtual std::string stringify() { return "1" }
+};
+
+class OpNegMock : public Base {
+	public:
+		OpNegMock() { }
+		virtual double evaluate() { return -1.0; }
+		virtual std::string stringify() { return "-1"; }
+};
+
+class OpZeroMock : public Base {
+	public:
+		OpZeroMock() { }
+		virtual double evaluate() { return 0.0; }
+		virtual std::string stringify() { return "0"; }
 };
 
 #endif //__OP_HPP__
