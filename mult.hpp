595823dac0eb19280aa715a9f9e39efc6b75ebae
abant006 <aaronbantay@gmail.com>
2021-11-04 12:50:36 -0700

    Adding the mult and mult_test classes

---
+++ mult.hpp
@@ -0,0 +1,19 @@
+#ifndef __MULT_HPP__
+#define __MULT_HPP__
+
+#include "base.hpp"
+#include "op.hpp"
+
+class Mult : public Base
+{
+	private:
+		Base* left;
+		Base* right;
+	public:
+		Mult(Base* left, Base* right) { this->left = left; this->right = right; }
+		virtual double evaluate() { return left->evaluate() * right->evaluate(); }
+		virtual std::string stringify() { return left->stringify() + " * " + right->stringify(); }
+};
+
+#endif //__MULT_HPP__
+
