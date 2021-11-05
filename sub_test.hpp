9273267632c232e45d12a6b639ccfbb389f13986
Frosa009 <91357123+Frosa009@users.noreply.github.com>
2021-11-05 16:37:29 -0700

    Update sub_test.hpp
    change expect to EXPECT

--- sub_test.hpp
+++ sub_test.hpp
@@ -6,55 +6,55 @@
 #include "op.hpp"
 
 TEST(Sub, subNegVal) {
-	op* v1 = new op(3);
-	op* v2 = new op(-7);
+	Op* v1 = new op(3);
+	Op* v2 = new op(-7);
 	Sub* test = new Sub (v1,v2);
-	Expect_EQ(test->evaluate(),10);
+	EXPECT_EQ(test->evaluate(),10);
 }
 
 TEST(Sub, subPosVal) {
-        op* v1 = new op(3);
-        op* v2 = new op(2);
+        Op* v1 = new op(3);
+        Op* v2 = new op(2);
         Sub* test = new Sub (v1,v2);
-        Expect_EQ(test->evaluate(),1);
+        EXPECT_EQ(test->evaluate(),1);
 }
 TEST(Sub, subNegNegVal) {
-        op* v1 = new op(-3);
-        op* v2 = new op(-7);
+        Op* v1 = new op(-3);
+        Op* v2 = new op(-7);
         Sub* test = new Sub (v1,v2);
-        Expect_EQ(test->evaluate(),4);
+        EXPECT_EQ(test->evaluate(),4);
 }
 TEST(Sub, subNegResult) {
-        op* v1 = new op(-10);
-        op* v2 = new op(3);
+        Op* v1 = new op(-10);
+        Op* v2 = new op(3);
         Sub* test = new Sub (v1,v2);
-        Expect_EQ(test->evaluate(),-13);
+        EXPECT_EQ(test->evaluate(),-13);
 }
 
 TEST(Sub, subZeroPVal) {
-        op* v1 = new op(2);
-        op* v2 = new op(0);
+        Op* v1 = new op(2);
+        Op* v2 = new op(0);
         Sub* test = new Sub (v1,v2);
-        Expect_EQ(test->evaluate(),2);
+        EXPECT_EQ(test->evaluate(),2);
 }
 
 TEST(Sub, subZeroNVal) {
-        op* v1 = new op(0);
-        op* v2 = new op(3);
+        Op* v1 = new op(0);
+        Op* v2 = new op(3);
         Sub* test = new Sub (v1,v2);
-        Expect_EQ(test->evaluate(),-3);
+        EXPECT_EQ(test->evaluate(),-3);
 }
 TEST(Sub, subNegValS) {
-        op* v1 = new op(3);
-        op* v2 = new op(7);
+        Op* v1 = new op(3);
+        Op* v2 = new op(7);
         Sub* test = new Sub (v1,v2);
-        Expect_EQ(test->stringify(),"3.000000 - 7.000000 ");
+        EXPECT_EQ(test->stringify(),"3.000000 - 7.000000 ");
 }
 TEST(Sub, subNegResultS) {
-        op* v1 = new op(-3);
-        op* v2 = new op(-7);
+        Op* v1 = new op(-3);
+        Op* v2 = new op(-7);
         Sub* test = new Sub (v1,v2);
-        Expect_EQ(test->stringify(),"-3.000000 - 7.000000 ");
+        EXPECT_EQ(test->stringify(),"-3.000000 - 7.000000 ");
 }
 
 #endif
